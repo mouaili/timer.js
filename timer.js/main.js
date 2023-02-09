7887801b58ec2e147975637307f49618d184f88a
Mohamed <77068169+mouaili@users.noreply.github.com>
2023-02-09 14:05:38 +0100

    Add files via upload

---
+++ timer.js/main.js
@@ -0,0 +1,15 @@
+const handleChronoTimer = timer => {
+  const time = Number(timer);
+  const h = Math.trunc(time / 3600).toFixed(0);
+  const m = (Math.trunc(time % 3600) / 60).toFixed(0);
+  const s = Math.trunc((time % 3600) % 60).toFixed(0);
+
+  const hour = h < 10 ? `0${h}` : h;
+  const minutes = m < 10 ? `0${m}` : m;
+  const seconds = s < 10 ? `0${s}` : s;
+
+  return `${hour}:${minutes}:${seconds}`;
+};
+handleChronoTimer(timer);
+
+console.log(handleChronoTimer(timer));
